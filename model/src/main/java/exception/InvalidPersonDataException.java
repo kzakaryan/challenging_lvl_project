2608643cce919aa937e761429280ce9c83fdd00a
kzakaryan <kzakaryan@griddynamics.com>
2025-01-13 03:21:15 +0400

    Removing Unnecessary Methods

--- model/src/main/java/exception/InvalidPersonDataException.java
+++ model/src/main/java/exception/InvalidPersonDataException.java
@@ -5,8 +5,4 @@ public class InvalidPersonDataException extends RuntimeException {
     public InvalidPersonDataException(String message) {
         super(message);
     }
-
-    public InvalidPersonDataException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
--- model/src/main/java/exception/PersonNotFoundException.java
+++ model/src/main/java/exception/PersonNotFoundException.java
@@ -5,8 +5,4 @@ public class PersonNotFoundException extends RuntimeException {
     public PersonNotFoundException(String message) {
         super(message);
     }
-
-    public PersonNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
